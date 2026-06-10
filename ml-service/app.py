7aa1ea5c5149eb8ce05229eaac1a4f6860dfb111
Banala Sai Lahari Reddy <laharixx908@gmail.com>
2026-06-10 19:34:11 +0530

    Update app.py

--- ml-service/app.py
+++ ml-service/app.py
@@ -12,10 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://sentivoice.vercel.app",
-        "http://localhost:5173"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
